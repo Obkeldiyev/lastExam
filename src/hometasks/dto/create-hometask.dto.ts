ed90d89d71936a724be451c9b3f768d20523a0c1
Obkeldiyev <obkeldiyevgiga13@gmail.com>
2024-10-30 12:20:25 +0500

    db done

--- src/hometasks/dto/create-hometask.dto.ts
+++ src/hometasks/dto/create-hometask.dto.ts
@@ -1,1 +1,11 @@
-export class CreateHometaskDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateHometaskDto {
+  @IsNotEmpty()
+  @IsString()
+  desc: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  lessonId: number;
+}
--- src/submission/entities/submission.entity.ts
+++ src/submission/entities/submission.entity.ts
@@ -1,1 +1,26 @@
-export class Submission {}
+import { Hometask } from 'src/hometasks/entities/hometask.entity';
+import { Student } from 'src/students/entities/student.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Submission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  content: string;
+
+  @OneToOne(() => Hometask)
+  @JoinTable()
+  hometask: Hometask;
+
+  @OneToOne(() => Student)
+  @JoinTable()
+  student: Student;
+}
